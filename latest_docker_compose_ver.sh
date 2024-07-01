17db8f93c3cb88c492027db374bb43e41f93ffe9
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-07-01 22:18:11 +0700

    Install latest version of Docker Compose on Ubuntu 20.04.06

---
+++ latest_docker_compose_ver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install Required Packages
+sudo apt update
+sudo apt install ca-certificates curl
+
+# Add Docker's Official GPG Key
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Set Up the Docker Repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Refresh the local pacakge
+sudo apt update
+
+# Install Docker Compose Plugin
+sudo apt install docker-compose-plugin
